de3ef2d2bc5cbed9af9023c29fe1fef4e5558f94
mark2020 <yansongmu2020@gmail.com>
2026-02-08 03:33:49 +0800

    add

---
+++ python/python_basics.py
@@ -0,0 +1,13 @@
+import hou
+
+# 打印 Houdini 版本确认身份
+print("--- Houdini Python 联通成功 ---")
+print(f"当前版本: {hou.applicationVersionString()}")
+
+# 尝试创建一个简单的节点（在内存中）
+node_count = len(hou.node("/").al ount}")
+
+print("--- 运行结束 ---")
+hou.pwd
+
+node = hou.pwd()
