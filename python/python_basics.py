defb3b444bf2656239a8aa6999d8470c7c08e8c1
mark2020 <yansongmu2020@gmail.com>
2026-02-08 05:20:17 +0800

    确认hython解释器连接成功

--- python/python_basics.py
+++ python/python_basics.py
@@ -5,13 +5,9 @@ print("--- Houdini Python 联通成功 ---")
 print(f"当前版本: {hou.applicationVersionString()}")
 
 # 尝试创建一个简单的节点（在内存中）
-<<<<<<< Updated upstream
 node_count = len(hou.node("/").allItems())
-=======
-node_count = len(hou.node("/").allNodes())
->>>>>>> Stashed changes
 
 print("--- 运行结束 ---")
-hou.pwd()
+hou.pwd
 
 node = hou.pwd()
